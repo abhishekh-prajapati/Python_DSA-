aa573b4aca53cfedaa682732c76867e354b412a9
abhishekh-prajapati <abhishekhprajapati9@gmail.com>
2026-04-25 21:13:11 +0530

    Q2: Convert Text Format

--- foundation_questions.py
+++ foundation_questions.py
@@ -1,6 +1,11 @@
 #Q1: First and Last Character + Reverse
-word =  "Developer"
-print(word[0])
-print(word[-1])
-print(word[::-1])
+# word =  "Developer"
+# print(word[0])
+# print(word[-1])
+# print(word[::-1])
+#
 
+#Q2: Convert Text Format
+text = ("python is easy")
+print(text.upper())
+print(text.title())
