03251b37dbb51add7a33e03efe701d6bedac52fc
abhishekh-prajapati <abhishekhprajapati9@gmail.com>
2026-04-28 21:39:25 +0530

    function,parameters with arguments one basic question solving

--- Functions_and_Arguments.py
+++ Functions_and_Arguments.py
@@ -5,8 +5,21 @@
 # greet()
 
 # Parameters
+#
+# def greet(name):
+#     print("Hello", name)
+# #Arguments
+# greet("Abhishekh")
 
-def greet(name):
-    print("Hello", name)
-#Arguments
-greet("Abhishekh")
+# Return
+# def add(a,b):
+#     return a + b
+# result = add(5, 3)
+# print(result)
+
+
+# Q1. Create a function named welcome() that prints:
+
+def welcome(name):
+    print("welcome", name)
+welcome(" to python")
