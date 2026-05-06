5515a0ed054b517d46a82c1fc82455b4f9ec14ca
abhishekh-prajapati <abhishekhprajapati9@gmail.com>
2026-05-06 23:36:02 +0530

    File Handling in python

---
+++ File_Handling.py
@@ -0,0 +1,8 @@
+#To read the file
+file = open("data.txt", "r")
+content = file.read()
+print(content)
+file.close()
+
+# data.txt file will contain hello world
+# as it contains hello world in the file
