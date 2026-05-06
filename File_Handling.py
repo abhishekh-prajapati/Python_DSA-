df17cef66da8b91508b3beaef144a51826fd5f06
abhishekh-prajapati <abhishekhprajapati9@gmail.com>
2026-05-06 23:37:27 +0530

    File Handling in python read function

--- File_Handling.py
+++ File_Handling.py
@@ -3,6 +3,12 @@ file = open("data.txt", "r")
 content = file.read()
 print(content)
 file.close()
-
 # data.txt file will contain hello world
 # as it contains hello world in the file
+
+#Readline
+file = open("data.txt","r")
+print(file.readline())
+print(file.readline())
+
+file.close()
