8be0290234557fc071ccef114375b0dc84a95ea5
abhishekh-prajapati <abhishekhprajapati9@gmail.com>
2026-05-09 23:28:06 +0530

    Json file writin

--- File_Handling.py
+++ File_Handling.py
@@ -36,50 +36,61 @@
 # #close()
 # file.close(z)
 # # Because open files consume system resources
+from test.test_win32trace import BasicSetupTearDown
 
-file = open("notes.txt", "w")
-file.write("python is easy")
-file.close()
+# file = open("notes.txt", "w")
+# file.write("python is easy")
+# file.close()
+#
+# #Practice 2
+# file = open("notes.txt","r")
+# print(file.read())
+# file.close()
+#
+# #append
+# # Practice 3
+# file = open("notes.txt","a")
+# file.write("\nI will master Python")
+# file.close()
+#
+#
+# #File handling with statement
+# #a New way to write this funciton (MODERN WAY)
+# with open("notes.txt", "r") as file:
+#     print(file.read())
+#
+#
+# #Practice 4
+# with open("Student.txt", "w") as file:
+#     file.write("Name: Rahul\n")
+#     file.write("Age: 20")
+#
+# ##CSV/TXT/JSON Basics
+#
+# # TXT Files
+# import csv
+#
+# with open("students.csv", "w",newline="")as file:
+#     writer = csv.writer(file)
+#
+#     writer.writerow(["name","age"])
+#     writer.writerow(["Rahul", 20])
+#     writer.writerow(["Aman", 22])
+#
+# #Reading CSV
+# import csv
+# with open("students.csv","r") as file:
+#     reader = csv.reader(file)
+#
+#     for row in reader:
+#         print(row)
 
-#Practice 2
-file = open("notes.txt","r")
-print(file.read())
-file.close()
-
-#append
-# Practice 3
-file = open("notes.txt","a")
-file.write("\nI will master Python")
-file.close()
-
-
-#File handling with statement
-#a New way to write this funciton (MODERN WAY)
-with open("notes.txt", "r") as file:
-    print(file.read())
-
-
-#Practice 4
-with open("Student.txt", "w") as file:
-    file.write("Name: Rahul\n")
-    file.write("Age: 20")
-
-##CSV/TXT/JSON Basics
-
-# TXT Files
-import csv
-
-with open("students.csv", "w",newline="")as file:
-    writer = csv.writer(file)
-
-    writer.writerow(["name","age"])
-    writer.writerow(["Rahul", 20])
-    writer.writerow(["Aman", 22])
-
-#Reading CSV
-import csv
-with open("students.csv","r") as file:
-    reader = csv.reader(file)
-
-    for row in reader:
-        print(row)
+# JSON Basic
+#Writing JSON
+import json
+data={
+    "name": "Rahul",
+    "age": 20
+}
+with open("data.json", "w") as file:
+    json.dump(data,file)
