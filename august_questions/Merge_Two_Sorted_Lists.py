0e54e11334340689c4590233ef8f69d7ed8efed9
Abhishekh prajapati <abhishekhprajapati9@gmail.com>
2026-09-01 15:45:38 +0530

    Update Merge_Two_Sorted_Lists.py

--- august_questions/Merge_Two_Sorted_Lists.py
+++ august_questions/Merge_Two_Sorted_Lists.py
@@ -1,7 +1,18 @@
-def list(list1, list2):
-  l1 = list1[0]
-  l2 = list2[0]
-  content = []
-  if l1 <= l2:
-    content.append(l1)
-    l1 +
+def merge(list1, list2):
+    i = 0
+    j = 0
+    content = []
+    while i < len(list1) and j < len(list2):
+        if list1[i] <= list2[j]:
+            content.append(list1[i])
+            i += 1
+        else:
+            content.append(list2[j])
+            j += 1
+    while i < len(list1):
+        content.append(list1[i])
+        i += 1
+    while j < len(list2):
+        content.append(list2[j])
+        j += 1
+    return content
